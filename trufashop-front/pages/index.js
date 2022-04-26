b1460a693abbfad86cef7be7ddee2e3e9f6381b2
rogeriogomes2009 <rogerio.gomes2009@gmail.com>
2022-04-26 16:13:02 -0300

    Update index.js

--- trufashop-front/pages/index.js
+++ trufashop-front/pages/index.js
@@ -1,23 +1,21 @@
 import Prismic from 'prismic-javascript'
-
-const CartIcon = () => (
-  <svg
-    xmlns='http://www.w3.org/2000/svg'
-    className='h-6 w-10'
-    fill='none'
-    viewBox='0 0 24 24'
-    stroke='currentColor'
-    strokeWidth={2}
-  >
-    <path
-      strokeLinecap='round'
-      strokeLinejoin='round'
-      d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
-    />
-  </svg>
-)
-
 const Index = (props) => {
+  const CartIcon = () => (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      className='h-6 w-10'
+      fill='none'
+      viewBox='0 0 24 24'
+      stroke='currentColor'
+      strokeWidth={2}
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
+      />
+    </svg>
+  )
   const { products } = props
   //const products = [1, 2, 3, 4, 5, 6]
   return (
@@ -34,7 +32,6 @@ const Index = (props) => {
                 d='M4 6h16M4 12h8m-8 6h16'
               />
             </div>
-
             <ul class='flex space-x-6'>
               <li class='text-white text-lg font-semibold tracking-normal cursor-pointer'>
                 Home
@@ -53,22 +50,23 @@ const Index = (props) => {
         </navbar>
       </header>
       <div className='h-screen bg-red-300'>
-        <h1 className='p-2'></h1>
-        <main className='grid grid-flow-col grid-cols-3 gap-1'>
+        <main className='grid grid-flow-col grid-cols-3 bg-red-700'>
           {products.map((product) => (
             <section className='flex flex-col md:flex-row py-10 px-5 bg-red-500 rounded-md shadow-lg'>
               <div className='text-indigo-500 flex flex-col justify-between'>
                 <img src={product.data.image.url} alt='' />
               </div>
               <div className='text-white'>
-                <small className='uppercase'>TRUFADOS</small>
-                <h3 className='uppercase text-black text-center text-2xl font-medium'>
+                <h4 className='uppercase text-lg text-right'>TRUFADOS</h4>
+                <h3 className='uppercase text-black text-lg font-medium text-right'>
                   {product.data.name}
                 </h3>
-                <h3 className='text-2xl font-semibold mb-7'>
-                  R$ {product.data.price}
-                </h3>
-                <div className='flex gap-0.5 mt-2 mb-0'>
+                <div>
+                  <h3 className='text-2xl font-semibold mb-7'>
+                    R$ {product.data.price}
+                  </h3>
+                </div>
+                <div className='flex gap-0.5 mt-0'>
                   <button
                     id='addToCartButton'
                     className='bg-indigo-600 hover:bg-indigo-500 focus:outline-none transition text-white uppercase px-8 py-3'
@@ -87,7 +85,7 @@ const Index = (props) => {
 export async function getServerSideProps({ res }) {
   const client = Prismic.client('https://trufashop2022.prismic.io/api/v2')
   const products = await client.query(
-    Prismic.Predicates.at('document.type', 'products')
+    Prismic.Predicates.at('document.type', 'product')
   )
   return {
     props: {
