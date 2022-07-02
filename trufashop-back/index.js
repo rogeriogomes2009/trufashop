1362442d04d673b6a5aec8af239116526d86a6cd
rogeriogomes2009 <rogerio.gomes2009@gmail.com>
2022-07-02 14:07:11 -0300

    upload various

--- trufashop-back/index.js
+++ trufashop-back/index.js
@@ -3,6 +3,7 @@ require('dotenv').config({ path: '../.env.producao' })
 const https = require('https')
 const fs = require('fs')
 const app = require('./app')
+const { createWebhook } = require('./lib/pix')
 
 const options = {
   //tls
@@ -20,4 +21,13 @@ const options = {
 }
 
 const server = https.createServer(options, app)
-server.listen(443)
+server.listen(
+  443,
+  () => (
+    console.log('server runing...'),
+    console.log('creating webhook for pix'),
+    createWebhook().then(() => {
+      console.log('webhook created')
+    })
+  )
+)
